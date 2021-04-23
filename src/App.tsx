67f0b9e7d6ec45a9dac5667435d534d6bd08fbf4
rivand <rivand@RIVAND1>
2021-04-23 20:48:30 -0300

    Added hook to save datas in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { BarChart } from './components/BarChart';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Table } from './components/Table';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
 import light from './styles/themes/light';
 import dark from './styles/themes/dark';
 import GlobalStyle from './styles/global';
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
+import usePersistedState from './hooks/usePersistedState';
 
 export function App() {
 
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
 
   const toggleTheme = useCallback(() => {
     setTheme(theme.title === 'light' ? dark : light)
